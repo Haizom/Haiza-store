26eba371b17168c376f7ece2794737d1d237d360
hazimhamid <hazimhamid5@gmail.com>
2023-10-14 17:06:50 +0500

    Navbar and topbar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Topbar from '../components/Topbar'
 
 export default function Home() {
   return (
     <div>
+        <Topbar />
         <Navbar />
     </div>
   )
