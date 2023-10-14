62d9c099eff0a3cedc355f9975738c9e343762dd
hazimhamid <hazimhamid5@gmail.com>
2023-10-15 02:46:20 +0500

    Footer

--- client/src/components/CategoryItem.jsx
+++ client/src/components/CategoryItem.jsx
@@ -1,10 +1,29 @@
 import styled from "styled-components";
 
+const Button = styled.button`
+    border:none;
+    padding: 10px;
+    background-color: white;
+    color:gray;
+    cursor: pointer;
+    font-weight: 600;
+`;
 const Container = styled.div`
   flex: 1;
   margin: 3px;
   height: 70vh;
   position: relative;
+  transition: all 0.5s ease;
+
+  &:hover ${Button} {
+    color: white;
+    background-color: black;
+  }
+
+  &:hover {
+    height: 60vh;
+    opacity: 0.8;
+  }
 `;
 
 const Image = styled.img`
@@ -30,14 +49,7 @@ const Title = styled.h1`
     margin-bottom: 20px;
 `;
 
-const Button = styled.button`
-    border:none;
-    padding: 10px;
-    background-color: white;
-    color:gray;
-    cursor: pointer;
-    font-weight: 600;
-`;
+
 
 const CategoryItem = ({ item }) => {
   return (
