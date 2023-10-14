cdfb5b743a2037fd98cea8cd520c8330bf411642
hazimhamid <hazimhamid5@gmail.com>
2023-10-14 18:34:49 +0500

    Slider

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Topbar from '../components/Topbar'
+import Slider from '../components/Slider'
 
 export default function Home() {
   return (
     <div>
         <Topbar />
         <Navbar />
+        <Slider />
     </div>
   )
 }
