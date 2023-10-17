04dbaeac4f4a68a5138551b4d30393571a9d01c3
hazimhamid <hazimhamid5@gmail.com>
2023-10-18 02:57:00 +0500

    Register and login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 // import Home from "./pages/Home";
 // import Products from "./pages/Products";
-import Product from "./pages/Product";
+// import Product from "./pages/Product";
+import Login from "./pages/Login";
+// import Register from "./pages/Register";
 
 function App() {
   return (
     <div className="App">
-      <Product />
+      <Login />
     </div>
   );
 }
