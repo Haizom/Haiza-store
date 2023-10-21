e4d46de2751b5692f3d0a1865d0668564b04c2e4
hazimhamid <hazimhamid5@gmail.com>
2023-10-21 15:29:02 +0500

    Cart page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 // import Home from "./pages/Home";
 // import Products from "./pages/Products";
 // import Product from "./pages/Product";
-import Login from "./pages/Login";
+// import Login from "./pages/Login";
 // import Register from "./pages/Register";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Cart />
     </div>
   );
 }
